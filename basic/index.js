4c9b412db19f0dffdcc14aaf06da7a8cb61e6fbc
Benjamim-EP <bannk18@gmail.com>
2022-12-04 10:25:49 -0300

    body-parser

--- basic/index.js
+++ basic/index.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 
-const saudacao = require('./saudacaoMid')
-app.use(saudacao)
+app.post('/corpo',(req,res)=>{
+	let corpo =''
+	req.on('data',function(parte){
+			corpo += parte
+	})
 
+	req.end('end',function(){
+		res.send(corpo)
+	})
+})
 
 app.listen(3001)
