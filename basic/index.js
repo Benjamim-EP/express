049f6ad90861b3cf07a311748dbacf5befbece06
Benjamim-EP <bannk18@gmail.com>
2022-12-04 11:04:04 -0300

    modulos usando o app

--- basic/index.js
+++ basic/index.js
@@ -2,10 +2,6 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 
-const usuarioApi = require('./api/usuario')
-
-app.post('/usuario', usuarioApi.salvar)
-app.get('/usuario', usuarioApi.obter)
-
+require('/api/produto')(app,'com param!')
 
 app.listen(3001)
